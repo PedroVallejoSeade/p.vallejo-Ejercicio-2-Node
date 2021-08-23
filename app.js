755987cb490fd84b958fa144ad8d5e6e3ef5ff40
UnfriendlyKoala <p.vallejo@uniandes.edu.co>
2021-08-23 10:15:28 -0500

    Proveedores terminado

--- app.js
+++ app.js
@@ -5,40 +5,27 @@ const {readFileSync, writeFileSync} = require('fs');
 const server = http.createServer((req, res) => {
 
     const url = req.url;
-    //Home page
-    if (url == '/') {
-        res.writeHead(200, { contentType: 'text/html' });
-        const principio = readFileSync('./templateBegin.html', 'utf8');
-        const mid = readFileSync('./templateMiddle.html', 'utf8');
-        const fin = readFileSync('./templateEnd.html', 'utf8');
-        writeFileSync('./prueba.html', principio);
-        writeFileSync('./prueba.html', mid, {flag: 'a'});
-        writeFileSync('./prueba.html', fin, {flag: 'a'});
-        const archivo = readFileSync('./prueba.html', 'utf8');
-        res.write(archivo);
-        res.end();
-    }
-    //Recurso de proveedores
-    else if (url == '/api/proveedores') {
+    //Provededores
+    if (url == '/api/proveedores') {
         axios.get('https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json')
             .then(function (response) {
                 // handle success
                 res.writeHead(200, { contentType: 'text/html' });
-                const principio = readFileSync('./templateBegin.html', 'utf8');
-                const fin = readFileSync('./templateEnd.html', 'utf8');
-                writeFileSync('./prueba.html', principio);
+                const principio = readFileSync('./templateBeginProveedores.html', 'utf8');
+                const fin = readFileSync('./templateEndProveedores.html', 'utf8');
+                writeFileSync('./proveedores.html', principio);
                 writeFileSync('./templateMiddle.html', '[');
                 response.data.forEach(function (proveedor){
                     const idProveedor = proveedor.idproveedor;
                     const nomCompania = proveedor.nombrecompania;
                     const nomContacto = proveedor.nombrecontacto;
-                    writeFileSync('./templateMiddle.html', `{ name: "${idProveedor}", age: "${nomCompania}", birthdate: "${nomContacto}" },` ,{flag: 'a'});
+                    writeFileSync('./templateMiddle.html', `{ id: "${idProveedor}", nombrecompania: "${nomCompania}", nombrecontacto: "${nomContacto}" },` ,{flag: 'a'});
                 })
                 writeFileSync('./templateMiddle.html', '];' ,{flag: 'a'});
                 const mid = readFileSync('./templateMiddle.html', 'utf8');
-                writeFileSync('./prueba.html', mid, {flag: 'a'});
-                writeFileSync('./prueba.html', fin, {flag: 'a'});
-                const archivo = readFileSync('./prueba.html', 'utf8');
+                writeFileSync('./proveedores.html', mid, {flag: 'a'});
+                writeFileSync('./proveedores.html', fin, {flag: 'a'});
+                const archivo = readFileSync('./proveedores.html', 'utf8');
                 res.write(archivo);
                 res.end();
             })
@@ -53,8 +40,8 @@ const server = http.createServer((req, res) => {
     }
     //404
     else {
-        //TODO
+        res.end('<h1> 404 :(</h>')
     }
 })
 
-server.listen(8080);
+server.listen(8081);
